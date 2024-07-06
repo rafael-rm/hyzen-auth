aa542153d01067592ab8e4aadc66b44d6e7db65b
rafael-rm <rafael.rmo@outlook.com>
2024-07-06 19:28:37 -0300

    Added PBKDF2

--- HyzenAuth.Core/Services/PasswordHelper.cs
+++ HyzenAuth.Core/Services/PasswordHelper.cs
@@ -1,55 +1,51 @@
 ﻿using System.Security.Cryptography;
-using System.Text;
 
-namespace HyzenAuth.Core.Services;
-
-public static class PasswordHelper
+namespace HyzenAuth.Core.Services
 {
-    private const int SaltSize = 32;
+    public static class PasswordHelper
+    {
+        private const int SaltSize = 64;
+        private const int HashSize = 64;
+        private const int Iterations = 256000;
 
-    public static string Hash(string password)
-    {
-        var saltBytes = GenerateSalt(SaltSize);
-        var passwordAndSaltBytes = Concatenate(password, saltBytes);
-        var hash = ComputeHash(passwordAndSaltBytes);
-        
-        var hashWithSalt = new byte[saltBytes.Length + hash.Length];
-        Buffer.BlockCopy(saltBytes, 0, hashWithSalt, 0, saltBytes.Length);
-        Buffer.BlockCopy(hash, 0, hashWithSalt, saltBytes.Length, hash.Length);
-        
-        return Convert.ToBase64String(hashWithSalt);
-    }
-        
-    public static bool Verify(string password, string storedHashWithSalt)
-    {
-        var hashWithSaltBytes = Convert.FromBase64String(storedHashWithSalt);
-        
-        var saltBytes = new byte[SaltSize];
-        Buffer.BlockCopy(hashWithSaltBytes, 0, saltBytes, 0, SaltSize);
-        
-        var hashBytes = new byte[hashWithSaltBytes.Length - SaltSize];
-        Buffer.BlockCopy(hashWithSaltBytes, SaltSize, hashBytes, 0, hashBytes.Length);
-        
-        var passwordAndSaltBytes = Concatenate(password, saltBytes);
-        var hashAttempt = ComputeHash(passwordAndSaltBytes);
-        return hashAttempt.SequenceEqual(hashBytes);
-    }
+        public static string Hash(string password)
+        {
+            var saltBytes = GenerateSalt(SaltSize);
+            var hashBytes = ComputeHash(password, saltBytes, Iterations, HashSize);
 
-    private static byte[] ComputeHash(byte[] bytes)
-    {
-        return SHA256.HashData(bytes);
-    }
-        
-    private static byte[] Concatenate(string password, byte[] saltBytes)
-    {
-        var passwordBytes = Encoding.UTF8.GetBytes(password);
-        return passwordBytes.Concat(saltBytes).ToArray();
-    }
+            var hashWithSalt = new byte[saltBytes.Length + hashBytes.Length];
+            Buffer.BlockCopy(saltBytes, 0, hashWithSalt, 0, saltBytes.Length);
+            Buffer.BlockCopy(hashBytes, 0, hashWithSalt, saltBytes.Length, hashBytes.Length);
 
-    private static byte[] GenerateSalt(int size)
-    {
-        var saltBytes = new byte[size];
-        RandomNumberGenerator.Fill(saltBytes);
-        return saltBytes;
+            return Convert.ToBase64String(hashWithSalt);
+        }
+
+        public static bool Verify(string password, string storedHashWithSalt)
+        {
+            var hashWithSaltBytes = Convert.FromBase64String(storedHashWithSalt);
+
+            var saltBytes = new byte[SaltSize];
+            Buffer.BlockCopy(hashWithSaltBytes, 0, saltBytes, 0, SaltSize);
+
+            var hashBytes = new byte[hashWithSaltBytes.Length - SaltSize];
+            Buffer.BlockCopy(hashWithSaltBytes, SaltSize, hashBytes, 0, hashBytes.Length);
+
+            var hashAttempt = ComputeHash(password, saltBytes, Iterations, HashSize);
+            return hashAttempt.SequenceEqual(hashBytes);
+        }
+
+        private static byte[] ComputeHash(string password, byte[] saltBytes, int iterations, int hashSize)
+        {
+            using var rfc2898 = new Rfc2898DeriveBytes(password, saltBytes, iterations, HashAlgorithmName.SHA512);
+            
+            return rfc2898.GetBytes(hashSize);
+        }
+
+        private static byte[] GenerateSalt(int size)
+        {
+            var saltBytes = new byte[size];
+            RandomNumberGenerator.Fill(saltBytes);
+            return saltBytes;
+        }
     }
 }
